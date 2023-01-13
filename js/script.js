dc019e84d686c0831eb56695e84be4356f2ff0e2
Anvarov Mirabror <abroranvarov04@gmail.com>
2023-01-13 15:59:54 +0300

    js homework:=

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let name="Abror"
+let surname="Anvarov"
+let petname="sikubidu"
+let age =16
+let birtYear= 2008
+console.log(name);
+console.log(surname);
+console.log(petname);
+console.log(age);
+console.log(birtYear);
+
+
+
